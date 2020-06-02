3f7f711d3ff824b66c20eb16474517a4eaca4e8b
chengj29 <chengj29@uw.edu>
2020-06-02 12:47:02 -0700

    checklist without user state

---
+++ clients/js/index.js
@@ -0,0 +1,86 @@
+// data to render
+const times = [ "12:00 - 12:30", "12:30 - 1:00", "1:00 - 1:30", "1:30 - 2:00", "2:00 - 2:30", "2:30 - 3:00", "3:00 - 3:30", "3:30 - 4:00", "4:00 - 4:30", "4:30 - 5:00", "5:00 - 5:30",
+"5:30 - 6:00", "6:00 - 6:30 ,","6:30 - 7:00","7:00 - 7:30" ,"7:30 - 8:00" ,"8:00 - 8:30" ,"8:30 - 9:00", "9:00 - 9:30","9:30 - 10:00" ,"10:00 - 10:30" ,"10:30 - 11:00" ,"11:00 - 11:30" ,"11:30 - 12:00","12:00 - 12:30" ,"12:30 - 1:00"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let state = {
+  selected: new Map(),
+};
+
+function renderOneDay(day) {
+  let dayArea = document.createElement("div");
+  dayArea.classList.add("day");
+  let nameofday = document.createElement("h5");
+  nameofday.innerHTML = day;
+  dayArea.appendChild(nameofday);
+  times.forEach(function (time) {
+    dayArea.appendChild(renderOneTime(time, "AM", day));
+  });
+  times.forEach(function (time) {
+    dayArea.appendChild(renderOneTime(time, "PM", day));
+  });
+  return dayArea;
+}
+
+function renderOneTime(time, amorpm, day) {
+  let oneTime = document.createElement("div");
+  oneTime.classList.add("form-group", "form-check");
+  let check = document.createElement("input");
+  check.type = "checkbox";
+  check.classList.add("form-check-input");
+  let parsedTime = time.split(" ");;
+  parsedTime = parsedTime[0].replace(":", "");
+  if (amorpm === "PM") {
+    parsedTime = parseInt(parsedTime) + 1200;
+    parsedTime = parsedTime.toString();
+  }
+  check.id = day + ":" + parsedTime;
+  if (state.selected.get(day).includes(parsedTime)){
+    check.checked = true;
+  }
+  oneTime.appendChild(check);
+  text = document.createElement("label");
+  text.classList.add("form-check-label");
+  text.for = "exampleCheck1";
+  text.innerHTML = time + " " + amorpm;
+  oneTime.appendChild(text);
+  return oneTime;
+}
+function renderOneWeek() {
+  let weekArea = document.querySelector("#schedule");
+  days.forEach(function(day) {
+    weekArea.appendChild(renderOneDay(day));
+  });
+  return weekArea;
+}
+function newMap(){
+  days.forEach(function(day){
+    state.selected.set(day, []);
+  });
+}
+function setState(){
+  if (state.selected.size === 0){
+    newMap();
+  }
+  //GET USER
+}
+function sendState(){
+  //POST /user/id
+}
+// call the function to render chats
+setState();
+renderOneWeek();
+let button = document.querySelector('#submit');
+button.addEventListener('click', () => {
+  newMap()
+  document.querySelectorAll(".form-check-input").forEach(function(checkmark){
+    if (checkmark.checked === true) {
+      let parsed = checkmark.id.split(":");
+      var arr = [];
+      arr = arr.concat(state.selected.get(parsed[0]));
+      arr.push(parsed[1]);
+      state.selected.set(parsed[0], arr);
+
+    }
+  });
+
+});
